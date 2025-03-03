4d34b5992a4a99e537599901b904e075242d5f8c
Xiaohua Peng <609370075@qq.com>
2025-03-03 22:49:26 +0800

    feat: 修改整体代码文件格式

--- src/types/enums.ts
+++
@@ -1,6 +0,0 @@
-export enum BlogType {
-  Web3 = 'web3',
-  EssayAndReading = 'essayAndReading',
-  Thinking = 'thinking',
-  Tech = 'tech'
-}
--- src/vite-env.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="vite/client" />
-
-declare interface Window {
-  __setPreferredTheme: (string) => void
-}
